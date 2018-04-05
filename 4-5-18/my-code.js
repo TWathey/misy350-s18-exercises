0cd4e4edd26e09ffe97c086a126479c0644a2d1a
Tyler Wathey <twathey@udel.edu>
2018-04-05 10:38:12 -0400

    added js examples

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,8 @@
+let blist = ["end of the semester", "no research", "Disen gets better"];
+console.log(blist.length);
+
+blist.push("get a beer");
+console.log(blist.length);
+
+console.log(blist.length);
+console.log(blist);

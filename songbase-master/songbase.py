eace21905cef2081405774bf4624ebe8c7912b0a
Tyler Wathey <twathey@udel.edu>
2018-05-08 10:11:41 -0400

    atom 5/8

--- songbase-master/songbase.py
+++ songbase-master/songbase.py
@@ -4,16 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        # return "hello world"
         return render_template('index.html')
 
 @app.route('/users/<string:username>')
 def users(username):
-    # return "<h1>hello %s</h1>" % username
     return render_template('user.html', uname=username)
+
 @app.route('/user')
 def user():
     return "this is the page for users"
+
+@app.route('/form')
+def form():
+        return render_template('form.html')
 
 if __name__ == '__main__':
     app.run()
--- tip.py
+++
@@ -1,9 +0,0 @@
-
-# get string input
-total = int(raw_input("Enter total amount: "))
-# get integer input: int() convert string to integer
-# float() convert string to floating number
-tip = float(raw_input("Enter Tip Rate (such as 0.15): "))
-total = total - total * tip
-# use string formatting to output result
-print "You should pay: $%s" % (total)
